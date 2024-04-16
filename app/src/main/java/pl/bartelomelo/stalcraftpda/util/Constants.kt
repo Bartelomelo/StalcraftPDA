b7101100101ba47774338834304c001250f33873
Bartelomelo <bartekmt14@gmail.com>
2024-04-16 19:16:23 +0200

    ItemCategoryScreen added and work on namespaces inventory.

--- app/src/main/java/pl/bartelomelo/stalcraftpda/util/Constants.kt
+++ app/src/main/java/pl/bartelomelo/stalcraftpda/util/Constants.kt
@@ -2,5 +2,5 @@ package pl.bartelomelo.stalcraftpda.util
 
 object Constants {
     const val BASE_DB_URL = "https://api.github.com/repos/EXBO-Studio/stalcraft-database/contents/global/"
-    const val API_TOKEN = "Bearer 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"
+    const val API_TOKEN = "Bearer TOKEN"
 }
